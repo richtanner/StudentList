ae99af2dab65d3ea37ceeec19bf01dd55983495a
jfk17a <44479985+jfk17a@users.noreply.github.com>
2019-09-06 08:39:48 -0500

    Kersker additions for switch, toast, and activity.

--- app/src/main/java/edu/cs315/studentlist/KerskerActivity.java
+++ app/src/main/java/edu/cs315/studentlist/KerskerActivity.java
@@ -3,12 +3,27 @@ package edu.cs315.studentlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class KerskerActivity extends AppCompatActivity {
+
+    ListView kerskerFacts;
+
+    String factsArray [] = {
+        "Two Older Sisters",
+        "Built Computers At 13",
+        "Learning Greek",
+        ""
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kersker);
+
+        kerskerFacts = (ListView)findViewById(R.id.language_list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, factsArray);
+        kerskerFacts.setAdapter(arrayAdapter);
     }
 }
