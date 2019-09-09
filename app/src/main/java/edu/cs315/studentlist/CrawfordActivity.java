f7085d0f8594f218621c6a999d88fb1d814140c3
Kelvin Crawford <kcc16a@zcu.edu>
2019-09-09 00:31:05 -0500

    Edited list and changed theme

---
+++ app/src/main/java/edu/cs315/studentlist/CrawfordActivity.java
@@ -0,0 +1,30 @@
+package edu.cs315.studentlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class CrawfordActivity extends AppCompatActivity {
+
+    ListView languageList;
+
+    String langArray[] = {
+            "Madden NFL 20",
+            "Star Wars Battlefront 2",
+            "Battlefield V",
+            "Warframe",
+            "UFC 3"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crawford);
+
+        languageList = findViewById(R.id.language_list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_listview, R.id.textView, langArray);
+        languageList.setAdapter(arrayAdapter);
+    }
+}
--- app/src/main/java/edu/cs315/studentlist/MainActivity.java
+++ app/src/main/java/edu/cs315/studentlist/MainActivity.java
@@ -79,5 +79,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             Intent intent = new Intent(context, TannerActivity.class);
             context.startActivity(intent);
         }
+
+        if (personSelected == "Kelvin Crawford") {
+            Context context = simpleList.getContext();
+            Intent intent = new Intent(context, CrawfordActivity.class);
+            context.startActivity(intent);
+        }
     }
 }
