4a55be2d633017cf17ab613ed20369e5031b9315
Felix Mbikogbia <mbikogbiafelix@gmail.com>
2019-09-09 07:59:19 -0500

    Added fab_studentList (Felix Mbikogbia).

--- app/src/main/java/edu/cs315/studentlist/FelixActivity.java
+++ app/src/main/java/edu/cs315/studentlist/FelixActivity.java
@@ -1,7 +1,10 @@
 package edu.cs315.studentlist;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,16 +14,15 @@ import android.widget.ListView;
 public class FelixActivity extends AppCompatActivity {
 
     ListView languageList;
+    ActionBar actionBar;
 
     // Array of strings...
     String langArray[] = {
             "HTML",
             "CSS",
-            "JQuery",
             "JavaScript",
-            "Java (Android (learning))",
-            "C++ (some)",
-            "Bootstrap"
+            "Java (some)",
+            "C++ (some)"
 
     };
 
@@ -30,24 +32,27 @@ public class FelixActivity extends AppCompatActivity {
         setContentView(R.layout.activity_felix);
 
         //back button here
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         languageList = (ListView)findViewById(R.id.language_list);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, langArray);
         languageList.setAdapter(arrayAdapter);
+
+//        actionBar = getSupportActionBar();
+//        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#286e9b")));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-
-        //his ends the FelixActivity page when the back button is clicked
-        if (id == android.R.id.home){
-            this.finish();
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        int id = item.getItemId();
+//
+//        //his ends the FelixActivity page when the back button is clicked
+//        if (id == android.R.id.home){
+//            this.finish();
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
 
