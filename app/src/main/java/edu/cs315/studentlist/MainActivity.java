5f3fe24f57e62a0196b7d0ccedb449f3406750df
sihong Guo <yellowjerry6@gmail.com>
2019-09-13 08:17:34 -0500

    Fix the top bar and custom my own color

--- app/src/main/java/edu/cs315/studentlist/MainActivity.java
+++ app/src/main/java/edu/cs315/studentlist/MainActivity.java
@@ -69,6 +69,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 // by specific index
                 Toast.makeText(this, personSelected + " wrote some jank code!!", Toast.LENGTH_LONG).show();
                 break;
+            case 8:
+                // by specific index
+                Toast.makeText(this, personSelected + " Falling Asleep!!!!", Toast.LENGTH_LONG).show();
+                break;
             default:
                 // every case that wasn't specifically called out
                 Toast.makeText(this, personSelected + " selected", Toast.LENGTH_LONG).show();
